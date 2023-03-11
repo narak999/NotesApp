fd80917a5e444bd937b7a5bd6708c982792c3755
BUNNARAK THENG <bunnarak.theng@gm.com>
2023-03-10 21:31:21 -0500

    Second commit

--- app/src/main/java/com/bunnarak/notes/NotesListActivity.java
+++ app/src/main/java/com/bunnarak/notes/NotesListActivity.java
@@ -3,12 +3,15 @@ package com.bunnarak.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class NotesListActivity extends AppCompatActivity {
+    private static final String TAG = "NotesListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: this is a test log!");
     }
 }
---
+++ app/src/main/java/com/bunnarak/notes/models/Note.java
@@ -0,0 +1,5 @@
+package com.bunnarak.notes.models;
+
+public class Note {
+
+}
