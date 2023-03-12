17a21312c4fdfa6f873d2f6211187ea9e743220a
BUNNARAK THENG <bunnarak.theng@gm.com>
2023-03-11 23:58:45 -0500

    Sixth commit

--- app/src/main/java/com/bunnarak/notes/NoteActivity.java
+++ app/src/main/java/com/bunnarak/notes/NoteActivity.java
@@ -2,27 +2,64 @@ package com.bunnarak.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.bunnarak.notes.models.Note;
 
 public class NoteActivity extends AppCompatActivity {
 
     private static final String TAG = "NoteActivity";
+
+    //UI components
+    private LinedEditText mLinedEditText;
+    private EditText mEditTitle;
+    private TextView mViewTitle;
+
+    //vars
+    private boolean mIsNewNote;
+    private Note mInitialNote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        mLinedEditText = findViewById(R.id.note_text);
+        mEditTitle = findViewById(R.id.note_edit_title);
+        mViewTitle = findViewById(R.id.note_text_title);
 
-        if(getIntent().hasExtra("selected_note")) {
-            Note note = getIntent().getParcelableExtra("selected_note");
-            Log.d(TAG, "onCreate: " + note.toString());
+        if (getIncomingIntent()) {
+            //if this is a new note, go to Edit Mode
+            setNewNoteProperties();
+        } else {
+            //if this is not a new note, go to View Mode
+            setNoteProperties();
         }
 
+    }
 
-        //View State
+    private boolean getIncomingIntent() {
+        if (getIntent().hasExtra("selected_note")) {
+            mInitialNote = getIntent().getParcelableExtra("selected_note");
+            Log.d(TAG, "getIncomingIntent: " + mInitialNote.toString());
+            mIsNewNote = false;
+            return false;
+        }
+        mIsNewNote = true;
+        return true;
+    }
 
-        //Edit State
+    private void setNoteProperties() {
+        mViewTitle.setText(mInitialNote.getTitle());
+        mEditTitle.setText(mInitialNote.getTitle());
+        mLinedEditText.setText(mInitialNote.getContent());
+    }
+
+    private void setNewNoteProperties() {
+        mViewTitle.setText("Note Title");
+        mEditTitle.setText("Note Title");
     }
 }
